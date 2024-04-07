7ab4cbb9188f129093e252fdb8bf2779a68a9325
Sebastian Lira <sebastianandresla16@gmail.com>
2024-04-07 00:05:54 -0400

    quitado el secret

--- clases/secrets_stripe.php
+++ clases/secrets_stripe.php
@@ -3,4 +3,4 @@
 // or in a private script that does not publicly expose the source code.
 
 // This is your test secret API key.
-$stripeSecretKey = 'sk_test_51P234HJ3YgtKbvo23v1Faa6MWDzNXWJC8y0dR5MfWEI7HpFHhCDrMUzAxxLuTjVcAZLk3xF6ufObalbpfBXBQ5zT00PhIF8522';
+// $stripeSecretKey = 'sk_test_51P234HJ3YgtKbvo23v1Faa6MWDzNXWJC8y0dR5MfWEI7HpFHhCDrMUzAxxLuTjVcAZLk3xF6ufObalbpfBXBQ5zT00PhIF8522';
