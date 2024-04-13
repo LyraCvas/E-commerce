d5b837eca22f8b2e4c6803dd78a7d610743fc399
MafernandaMeneses <mafernanda-22@hotmail.com>
2024-04-13 08:08:19 -0400

    logout

--- menu.php
+++ menu.php
@@ -1,3 +1,6 @@
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+
 <header>
         <div class="header__superior">
             <div class="logo">
@@ -13,6 +16,7 @@
             </div>
 
             <?php if(isset($_SESSION['user_id'])){ ?>
+                
                 
                 
                 <div class="dropdown_mar">
